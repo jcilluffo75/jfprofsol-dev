8b5ae98772e0259d060cc9bce71cba5f9d34902d
jcilluffo75 <cilluffojohn@gmail.com>
2025-09-22 15:32:05 -0400

    Add files via upload

---
+++ FunctionApp/BlobTriggerFunction/FunctionApp.py
@@ -0,0 +1,12 @@
+import logging
+import azure.functions as func
+
+def main(blob: func.InputStream):
+    logging.info(f"Processing blob: {blob.name}, Size: {blob.length} bytes")
+
+    content = blob.read().decode('utf-8')
+    
+    # TODO: Parse EDI/CSV/Excel content
+    # TODO: Insert into SQL tables: edi_835_raw, edi_835_claims, edi_835_services
+
+    logging.info("Blob processed successfully.")
